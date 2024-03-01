2af41f7a7e8876980ef73b4c2b32917a1c8fc62c
Ryan Lima <contato@ryanlima.dev>
2024-03-01 14:00:38 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,165 @@
-import Image from "next/image";
+'use client'
+
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {getInstallments, initMercadoPago} from '@mercadopago/sdk-react'
+import {createCardToken} from '@mercadopago/sdk-react/coreMethods'
+import {useEffect, useState} from "react";
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    initMercadoPago('TEST-059b6f26-9a09-4f97-a931-4ca801e64171');
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const [cardNumber, setCardNumber] = useState<string>('');
+    const [transactionAmount, setTransactionAmount] = useState<float>(0);
+    const [email, setEmail] = useState<string>('');
+    const [cardHolder, setCardHolder] = useState<string>('');
+    const [identificationType, setIdentificationType] = useState<string>('');
+    const [identificationNumber, setIdentificationNumber] = useState<string>('');
+    const [installmentsPayload, setInstallmentsPayload] = useState();
+    const [installments, setInstallments] = useState<int>(1);
+    const [installmentOptions, setInstallmentOptions] = useState<int>(1);
+    const [expirationMonth, setExpirationMonth] = useState<int>();
+    const [expirationYear, setExpirationYear] = useState<int>();
+    const [cvv, setCvv] = useState<int>();
+    const [paymentMethodId, setPaymentMethodId] = useState<int>();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    useEffect(() => {
+        const x = async () => {
+            const sanitizedCardNumber = cardNumber.replace(/[^0-9]/g, '');
+            if (transactionAmount !== 0 && cardNumber !== '' && sanitizedCardNumber.length >= 8) {
+                setInstallmentsPayload({
+                    payload: {
+                        amount: transactionAmount, bin: sanitizedCardNumber.substring(0, 8)
+                    }, hasChanged: true,
+                });
+            }
+        }
+        x();
+    }, [cardNumber, transactionAmount])
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    const updateInstallmentOptions = async () => {
+        try {
+            if (installmentsPayload !== undefined && installmentsPayload.hasChanged) {
+                const response = await getInstallments(installmentsPayload.payload)
+                setInstallmentOptions(response[0].payer_costs)
+                setPaymentMethodId(response[0].payment_method_id)
+                installmentsPayload.hasChanged = false;
+            }
+        } catch (e) {
+        }
+    }
+
+    const handleFormSubmit = async(e) => {
+
+        e.preventDefault()
+
+        const requestBody = {
+            transaction_amount: transactionAmount,
+            installments: installments,
+            token: (await createCardToken({
+                cardNumber: cardNumber.replace(/[^0-9]/g, ''),
+                cardholderName: cardHolder,
+                cardExpirationMonth: expirationMonth,
+                cardExpirationYear: expirationYear,
+                securityCode: cvv,
+                identificationType: identificationType,
+                identificationNumber: identificationNumber,
+            })).id,
+            payment_method_id: paymentMethodId,
+            payer: {
+                email: email,
+                identification: {
+                    type: identificationType,
+                    number: identificationNumber,
+                }
+            }
+        };
+
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/2023.5.8'},
+            body: JSON.stringify(requestBody)
+        };
+
+        const response = await fetch('http://localhost:9501/rest/payments', options)
+
+        if (response.status === 201) {
+            alert('Pagamento registrado com sucesso!');
+            location.reload()
+        }
+
+    }
+
+    return (<main className="justify-center min-h-screen p-8">
+        <form onSubmit={handleFormSubmit} className={"grid grid-cols-1"} method={"POST"}>
+            <div className="grid grid-cols-2 gap-8">
+                <Card className="">
+                    <CardHeader>
+                        <h3 className="font-bold">Dados do pagador</h3>
+                    </CardHeader>
+                    <CardContent>
+                        <Input onChange={(event) => setEmail(event.target.value)} className="mb-2 mt-2" type={"text"}
+                               name={"email"} id={"email"} placeholder={"Email"}></Input>
+                        <Select onValueChange={(event) => setIdentificationType(event)}>
+                            <SelectTrigger className="mb-2 mt-2">
+                                <SelectValue placeholder="Tipo de identificação"/>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="CPF">CPF</SelectItem>
+                                <SelectItem value="CNPJ">CNPJ</SelectItem>
+                            </SelectContent>
+                        </Select>
+                        <Label htmlFor={"identificationNumber"}>
+                            <Input onChange={(event) => setIdentificationNumber(event.target.value)} type={"text"}
+                                   name={"identificationNumber"} id={"identificationNumber"}
+                                   placeholder="Número de identificação"></Input>
+                        </Label>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <h3 className="font-bold">Dados do pagamento</h3>
+                    </CardHeader>
+                    <CardContent>
+                        <Input onChange={(event) => setTransactionAmount(event.target.value)} className="mb-2 mt-2"
+                               type={"float"} placeholder={"Valor do pagamento"}></Input>
+                        <Input onChange={(event) => setCardNumber(event.target.value)} className="mb-2 mt-2"
+                               type={"text"} placeholder={"Número do cartão"}></Input>
+                        <Input onChange={(event) => setCardHolder(event.target.value)} className="mb-2 mt-2" type={"text"}
+                               placeholder={"Nome do titular"}></Input>
+                        <Input onChange={(event) => setExpirationMonth(event.target.value)} className="mb-2 mt-2" type={"number"}
+                               placeholder={"Mês de inspiração (MM)"}></Input>
+                        <Input onChange={(event) => setExpirationYear(event.target.value)} className="mb-2 mt-2" type={"number"}
+                               placeholder={"Nome do titular (YYYY)"}></Input>
+                        <Input onChange={(event) => setCvv(event.target.value)} className="mb-2 mt-2" type={"number"}
+                               placeholder={"CVV"}></Input>
+                        <select
+                            defaultValue={'0'}
+                            onChange={(event) => {
+                                setInstallments(event.target.value);
+                            }}
+                            onMouseEnter={updateInstallmentOptions}
+                            className={"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}>
+                            <option value={'0'} onChange={(e) => setInstallments(e.target.value)}>Selecione uma
+                                opção
+                            </option>
+                            {installmentOptions && installmentOptions.length > 0 ? installmentOptions.map((option, key) => (
+                                <option key={key}
+                                        value={option.installments}
+                                >
+                                    {option.recommended_message}
+                                </option>)) : <option value={'0'}>Trabalhando nisso...</option>}
+                        </select>
+                    </CardContent>
+                </Card>
+            </div>
+            <Button className="mt-4">Pagar</Button>
+        </form>
+    </main>);
 }
